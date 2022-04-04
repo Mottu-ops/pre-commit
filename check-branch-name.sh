cf69cd2bea4a6683ea702743ef8bdf075b13968d
Luiz André Carvalho <luizandre.ita@gmail.com>
2022-04-04 17:46:09 -0300

    Update check-branch-name.sh

--- check-branch-name.sh
+++ check-branch-name.sh
@@ -6,8 +6,8 @@ valid_branch_regex='^((feature|bug|us|hotfix|release|issue)\/[0-9\-]+)|(main)|(d
 message="There is something wrong with your branch name. Branch names in this project must adhere to this contract: $valid_branch_regex. Your commit will be rejected. You should rename your branch to a valid name and try again."
 
 if [[ ! $local_branch_name =~ $valid_branch_regex ]]; then
-    echo "$message"
+    tput setaf 1; echo "$message"
     exit 1
 fi
-
+tput setaf 1; echo "Correct branch name!"
 exit 0
