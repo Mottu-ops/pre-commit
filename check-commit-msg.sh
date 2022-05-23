2f20fd893443cd764805168975cad04f1415ae19
Luiz André Carvalho <luizandre.ita@gmail.com>
2022-05-23 14:41:55 -0300

    Update check-commit-msg.sh

--- check-commit-msg.sh
+++ check-commit-msg.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-local_commit=$(git log -1 HEAD --pretty=format:%s)
 valid_commit_regex='(#[0-9]+ \[(fix|feat|chore|style|perf|build|refactor|test|docs)+\] - [a-zA-Z])'
 
-message="There is something wrong with your commit, message: $local_commit. Commits in this project must adhere to this agreement: $valid_commit_regex. Your commit will be rejected. You should rename your commit to a valid name and try again."
+message="There is something wrong with your commit. Commits in this project must adhere to this agreement: $valid_commit_regex. Your commit will be rejected. You should rename your commit to a valid name and try again."
 
-if  [[ ! $local_commit =~ $valid_commit_regex ]]; then
+if ! grep -iqE "$valid_commit_regex" "$1"; then
     tput setaf 1;
     echo "$message"
     tput sgr0
