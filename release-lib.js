877bf916079e17380d5b96dd1572e4a3074a2bc6
Luiz André Carvalho <luizandre.ita@gmail.com>
2024-08-06 19:28:43 -0300

    Update release-lib.js

--- release-lib.js
+++ release-lib.js
@@ -3,7 +3,6 @@ const { exec, execSync } = require('child_process');
 const name = process.argv[2];
 const arg = process.argv[3] || 'patch';
 
-console.log("process", process.argv);
 console.log(name, arg);
 
 if (!name) {
@@ -12,23 +11,13 @@ if (!name) {
 
 if (arg === 'patch' || arg === 'minor' || arg === 'major' || arg === 'preminor' || arg === 'premajor' || arg === 'prepatch' || arg === 'prerelease') {
 	console.log('Deploy version:', arg);
+	execSync(`git pull && ng build ${name} && npm run lint`, { stdio: [0, 1, 2] });
 
-	exec('git rev-parse --abbrev-ref HEAD', (err, stdout) => {
-		if (err) {
-			throw new Error('Error: checking branch');
-		}
+	execSync(`cd projects/${name} && npm version ${arg} && cd ../..`, { stdio: [0, 1, 2] });
 
-		if (typeof stdout === 'string' && (stdout.trim() === 'main' && arg === 'patch' || arg === 'minor' || arg === 'major')) {
-			execSync(`git pull && ng build ${name} && npm run lint`, { stdio: [0, 1, 2] });
+	execSync('git add . && git commit --amend --no-edit', { stdio: [0, 1, 2] });
 
-			execSync(`cd projects/${name} && npm version ${arg} && cd ../..`, { stdio: [0, 1, 2] });
-
-			execSync('git add . && git commit --amend --no-edit', { stdio: [0, 1, 2] });
-
-			execSync('npm version ' + arg, { stdio: [0, 1, 2] });
-		} else {
-			throw new Error('Error: The branch used must be main');
-		}
+	execSync('npm version ' + arg, { stdio: [0, 1, 2] });
 	});
 } else {
 	throw new Error('Error: semantic versioning not supported, use "major", "minor", "patch" or "premajor", "preminor", "prepatch" or "prerelease"');
