73ac2233e6e7d803de84bc1dcca74d154259e2b3
Luiz André Carvalho <luizandre.ita@gmail.com>
2024-08-06 19:35:55 -0300

    Update release-lib.js

--- release-lib.js
+++ release-lib.js
@@ -11,7 +11,7 @@ if (!name) {
 
 if (arg === 'patch' || arg === 'minor' || arg === 'major' || arg === 'preminor' || arg === 'premajor' || arg === 'prepatch' || arg === 'prerelease') {
 	console.log('Deploy version:', arg);
-	execSync(`git pull && ng build ${name} && npm run lint`, { stdio: [0, 1, 2] });
+	execSync(`git pull && npm run lint`, { stdio: [0, 1, 2] });
 
 	execSync(`cd projects/${name} && npm version ${arg} && cd ../..`, { stdio: [0, 1, 2] });
 
